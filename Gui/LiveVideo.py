b193266941401fb62541035364b0a6a72a054e50
Alec May <Alecmay101@gmail.com>
2018-03-03 14:50:36 -0500

    Add screenshot functionality
    Added screenshot functionality via button. Added quit button. Fixed bug with escape not killing program correctly

--- Gui/LiveVideo.py
+++ Gui/LiveVideo.py
@@ -1,26 +1,52 @@
 import tkinter as tk
 import cv2
+import os
+import datetime
 from PIL import Image, ImageTk
 
+# Initialization
+path = r'C:\Users\Alec\Desktop\HackFSU\Images'
 width, height = 800, 600
+
+# Initializes the video capture
 cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
 
+# Initializes the GUI
 root = tk.Tk()
-root.bind('<Escape>', lambda e: root.quit())
-lmain = tk.Label(root)
-lmain.pack()
+vidFeed = tk.Label(root)
+vidFeed.pack()
 
-def show_frame():
+# Binds escape to exit the program
+root.bind('<Escape>', lambda e: root.destroy())
+
+# Captures a frame and displays it
+def showFrame():
     _, frame = cap.read()
-    frame = cv2.flip(frame, 1)
     cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
     img = Image.fromarray(cv2image)
     imgtk = ImageTk.PhotoImage(image=img)
-    lmain.imgtk = imgtk
-    lmain.configure(image=imgtk)
-    lmain.after(10, show_frame)
+    vidFeed.imgtk = imgtk
+    vidFeed.configure(image=imgtk)
+    vidFeed.after(10, showFrame)
+	
+# Takes the screenshot and saves it
+def screenshot():
+    ts = datetime.datetime.now()
+    filename = "{}.jpg".format(ts.strftime("%Y-%m-%d_%H-%M-%S"))
+    p = os.path.join(path,filename)
+    _, frame = cap.read()
+    cv2.imwrite(p, frame)
+	
+# Sets up the button for taking a screenshot	
+picBtn = tk.Button(root, text="Snapshot", command=screenshot)
+picBtn.pack(fill="both", expand=True, padx=200, pady=5)
 
-show_frame()
+# Sets up the quit button
+quitBtn = tk.Button(root, text="Quit", command=root.destroy)
+quitBtn.pack(fill="both", expand=True, padx=350, pady=10)
+
+# Displays the current frame
+showFrame()
 root.mainloop()
