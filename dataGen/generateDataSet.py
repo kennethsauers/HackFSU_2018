547983eb7e4b29d9eee875e8384aa54e1f7a1be4
Matthew Villegas <matthew.villegas@knights.ucf.edu>
2018-03-03 16:32:18 -0500

    added functionality to provide size of set
    Can specify how many images are to be generated

--- dataGen/generateDataSet.py
+++ dataGen/generateDataSet.py
@@ -1,13 +1,13 @@
 import cv2
 import random
 
-def rotateBaseImg(imgFile):
+def rotateBaseImg(imgFile, setSize):
     degreeOffset = 10
     totalImgs = []
     quarterNote = cv2.imread(imgFile, 0)
     quarterNote = cv2.bitwise_not(quarterNote)
     rows,cols = quarterNote.shape
-    for i in range(5):
+    for i in range(setSize-2):
         M = cv2.getRotationMatrix2D((cols/2,rows/2),random.randint(-degreeOffset,degreeOffset),1)
         curImage = cv2.warpAffine(quarterNote,M,(cols,rows))
         curImage = cv2.bitwise_not(curImage)
@@ -18,7 +18,7 @@ def rotateBaseImg(imgFile):
     totalImgs.append(curImage)
     quarterNote = cv2.bitwise_not(quarterNote)
     totalImgs.append(quarterNote)
-    for i in range(7):
+    for i in range(setSize):
     	cv2.imshow('rotation' + str(i), totalImgs[i])
 
     cv2.waitKey(0)
@@ -26,6 +26,6 @@ def rotateBaseImg(imgFile):
 
 def main():
 	#filename = input('Enter file for base image: ')
-	rotateBaseImg('quarterNoLine.png')
+	rotateBaseImg('quarterNoLine.png', 7)
 if __name__ == '__main__':
 	main()
