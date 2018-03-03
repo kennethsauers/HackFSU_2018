ff81413981b789deb0faea6964ef9e3c7e74d4d4
Alec May <Alecmay101@gmail.com>
2018-03-03 16:18:47 -0500

    Make buttons smaller

--- GUI/VideoGui.py
+++ GUI/VideoGui.py
@@ -17,6 +17,8 @@ displayFactor = 2
 displayWidth = outputWidth//displayFactor
 displayHeight = outputHeight//displayFactor
 cap = cv2.VideoCapture(0)
+
+# Sets the resolution of the camera lower to keep the window small
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, displayWidth)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, displayHeight)
 
@@ -25,8 +27,10 @@ root = tk.Tk()
 vidFeed = tk.Label(root)
 vidFeed.pack()
 
+# Increases the resolution that the camera records
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, outputWidth)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, outputHeight)
+
 # Binds escape to exit the program
 root.bind('<Escape>', lambda e: root.destroy())
 
@@ -51,11 +55,11 @@ def screenshot():
 	
 # Sets up the button for taking a screenshot	
 picBtn = tk.Button(root, text="Snapshot", command=screenshot)
-picBtn.pack(fill="both", expand=True, padx=200, pady=5)
+picBtn.pack(fill="both", expand=True, padx=350, pady=5)
 
 # Sets up the quit button
 quitBtn = tk.Button(root, text="Quit", command=root.destroy)
-quitBtn.pack(fill="both", expand=True, padx=350, pady=10)
+quitBtn.pack(fill="both", expand=True, padx=450, pady=10)
 
 # Displays the current frame
 showFrame()
