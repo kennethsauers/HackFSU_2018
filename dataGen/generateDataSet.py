81d656fe780a42ae44c8f266c5d828c7b6ab5018
Alec May <Alecmay101@gmail.com>
2018-03-03 22:28:04 -0500

    Make better names for label numpy
    Invert BW

--- dataGen/generateDataSet.py
+++ dataGen/generateDataSet.py
@@ -4,6 +4,7 @@ import os
 import numpy as np
 import datetime
 
+# Generates the numpy for the non-note images
 def generateEmptyLabels(count):
     print('Generating Empty Lables...')
     folderName = 'DataSet'
@@ -13,15 +14,17 @@ def generateEmptyLabels(count):
     if not os.path.exists(path):
 	    os.makedirs(path)
 	
+	# Fills the top row with 1, and the bottom row with 0	
     lilBoi = [[1 for x in range(int (count))]for y in range(2)]
     for i in range(int(count)):
-        lilBoi[0][i] = 0
+        lilBoi[1][i] = 0
     lilBoi = np.asarray(lilBoi)
-
+	
     fileName = ("EmptyLabels%s" % count)
     filePath = os.path.join(path, fileName)
     np.save(filePath, lilBoi, allow_pickle=True, fix_imports=True) 
 
+# Generates the numpy of labels for quarter notes	
 def generateQuarterLabels(count):
     print('Generating Quarter Labels...')
     folderName = 'DataSet'
@@ -30,18 +33,22 @@ def generateQuarterLabels(count):
     if not os.path.exists(path):
 	    os.makedirs(path)
 
+	# Fills the top row with 0, and the bottom row with 1	
     lilBoi = [[0 for x in range(int(count))]for y in range(2)]
     for i in range(int(count)):
-        lilBoi[0][i] = 1
+        lilBoi[1][i] = 1
     lilBoi = np.asarray(lilBoi)
 
     fileName = ("QuarterLabels%s" % count)
     filePath = os.path.join(path, fileName)
     np.save(filePath, lilBoi, allow_pickle=True, fix_imports=True) 
 
+# Creates the set of slightly rotated and translated images
 def createSet(imgFile, setSize):
-    degreeOffset = 10
+    # Sets realistic boundaries for how different the picture will be
+	degreeOffset = 10
     translationOffset = 5;
+	
     totalImgs = []
     quarterNote = cv2.imread(imgFile, 0)
     quarterNote = cv2.bitwise_not(quarterNote)
@@ -51,23 +58,24 @@ def createSet(imgFile, setSize):
         curImage = cv2.warpAffine(quarterNote,M,(cols,rows))
         M = np.float32([[1,0,random.randint(-translationOffset,translationOffset)],[0,1,random.randint(-translationOffset,translationOffset)]])
         curImage = cv2.warpAffine(curImage,M,(cols,rows))
-        curImage = cv2.bitwise_not(curImage)
+        #curImage = cv2.bitwise_not(curImage)
         totalImgs.append(curImage)
     M = cv2.getRotationMatrix2D((cols/2,rows/2),180,1)
     curImage = cv2.warpAffine(quarterNote,M,(cols,rows))
     M = np.float32([[1,0,random.randint(-translationOffset,translationOffset)],[0,1,random.randint(-translationOffset,translationOffset)]])
     curImage = cv2.warpAffine(curImage,M,(cols,rows))
-    curImage = cv2.bitwise_not(curImage)
+    #curImage = cv2.bitwise_not(curImage)
     totalImgs.append(curImage)
-    quarterNote = cv2.bitwise_not(quarterNote)
+    #quarterNote = cv2.bitwise_not(quarterNote)
     totalImgs.append(quarterNote)
     if imgFile == 'quarterNoLine.png':
     	generateQuarterLabels(setSize)
     else:
     	generateEmptyLabels(setSize)
-    saveImgs(totalImgs)
-    #drawImgs(totalImgs, setSize)
-   
+    #saveImgs(totalImgs, imgFile)
+    drawImgs(totalImgs, setSize)
+
+# Draws the resultant images on the screen	
 def drawImgs(imgList, setSize):
     for i in range(setSize):
         cv2.imshow('rotation' + str(i), imgList[i])
@@ -75,14 +83,18 @@ def drawImgs(imgList, setSize):
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-def saveImgs(dataSet):
+# Saves the resultant images	
+def saveImgs(dataSet, imgFile):
     folderName = 'DataSet'
     path = os.path.join(os.getcwd(), folderName)
-    ts = datetime.datetime.now()
+    size = len(dataSet)
     if not os.path.exists(path):
         os.makedirs(path)
     bigBoi = np.asarray(dataSet)
-    fileName = "BigBoi{}".format(ts.strftime("%d_%H-%M-%S"))
+    if imgFile == 'quarterNoLine.png':
+	    fileName = ("QuarterNotes%s" % size)
+    else:
+	    fileName = ("EmptyNotes%s" % size)
     filePath = os.path.join(path, fileName)
     np.save(filePath, bigBoi, allow_pickle=True, fix_imports=True)      
         
