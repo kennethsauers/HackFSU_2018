634bf0bcb380b121f1791c86e23455eebd34c164
Alec May <Alecmay101@gmail.com>
2018-03-03 21:37:42 -0500

    Take variable number of labels

--- dataGen/BigBoiInvalidLabel.py
+++ dataGen/BigBoiInvalidLabel.py
@@ -7,9 +7,16 @@ import datetime
 folderName = 'DataSet'
 path = os.path.join(os.getcwd(), folderName)
 ts = datetime.datetime.now()
+count = input("How many labels do you need? ")
+
 if not os.path.exists(path):
 	os.makedirs(path)
-lilBoi = np.zeros(100)
-fileName = "BigBoiInvalidLabel"
+	
+lilBoi = [[1 for x in range(int (count))]for y in range(2)]
+for i in range(int(count)):
+    lilBoi[0][i] = 0
+lilBoi = np.asarray(lilBoi)
+
+fileName = ("EmptyLabels%s" % count)
 filePath = os.path.join(path, fileName)
 np.save(filePath, lilBoi, allow_pickle=True, fix_imports=True) 
--- dataGen/BigBoiValidLabel.py
+++ dataGen/BigBoiValidLabel.py
@@ -7,9 +7,15 @@ import datetime
 folderName = 'DataSet'
 path = os.path.join(os.getcwd(), folderName)
 ts = datetime.datetime.now()
+count = input("How many labels do you need? ")
 if not os.path.exists(path):
 	os.makedirs(path)
-lilBoi = np.ones(100)
-fileName = "BigBoiLabelValid"
+
+lilBoi = [[0 for x in range(int(count))]for y in range(2)]
+for i in range(int(count)):
+    lilBoi[0][i] = 1
+lilBoi = np.asarray(lilBoi)
+
+fileName = ("QuarterLabels%s" % count)
 filePath = os.path.join(path, fileName)
 np.save(filePath, lilBoi, allow_pickle=True, fix_imports=True) 
