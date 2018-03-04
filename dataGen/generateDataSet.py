a21b197d85bf32e9ed74eb346955bf577718030c
Matthew Villegas <matthew.villegas@knights.ucf.edu>
2018-03-03 21:54:10 -0500

    combine generate data and labels

--- dataGen/generateDataSet.py
+++ dataGen/generateDataSet.py
@@ -4,6 +4,41 @@ import os
 import numpy as np
 import datetime
 
+def generateEmptyLabels(count):
+    print('Generating Empty Lables...')
+    folderName = 'DataSet'
+    path = os.path.join(os.getcwd(), folderName)
+    ts = datetime.datetime.now()
+
+    if not os.path.exists(path):
+	    os.makedirs(path)
+	
+    lilBoi = [[1 for x in range(int (count))]for y in range(2)]
+    for i in range(int(count)):
+        lilBoi[0][i] = 0
+    lilBoi = np.asarray(lilBoi)
+
+    fileName = ("EmptyLabels%s" % count)
+    filePath = os.path.join(path, fileName)
+    np.save(filePath, lilBoi, allow_pickle=True, fix_imports=True) 
+
+def generateQuarterLabels(count):
+    print('Generating Quarter Labels...')
+    folderName = 'DataSet'
+    path = os.path.join(os.getcwd(), folderName)
+    ts = datetime.datetime.now()
+    if not os.path.exists(path):
+	    os.makedirs(path)
+
+    lilBoi = [[0 for x in range(int(count))]for y in range(2)]
+    for i in range(int(count)):
+        lilBoi[0][i] = 1
+    lilBoi = np.asarray(lilBoi)
+
+    fileName = ("QuarterLabels%s" % count)
+    filePath = os.path.join(path, fileName)
+    np.save(filePath, lilBoi, allow_pickle=True, fix_imports=True) 
+
 def createSet(imgFile, setSize):
     degreeOffset = 10
     translationOffset = 5;
@@ -26,9 +61,13 @@ def createSet(imgFile, setSize):
     totalImgs.append(curImage)
     quarterNote = cv2.bitwise_not(quarterNote)
     totalImgs.append(quarterNote)
+    if imgFile == 'quarterNoLine.png':
+    	generateQuarterLabels(setSize)
+    else:
+    	generateEmptyLabels(setSize)
     saveImgs(totalImgs)
-   # drawImgs(totalImgs, setSize)
-
+    #drawImgs(totalImgs, setSize)
+   
 def drawImgs(imgList, setSize):
     for i in range(setSize):
         cv2.imshow('rotation' + str(i), imgList[i])
@@ -48,9 +87,9 @@ def saveImgs(dataSet):
     np.save(filePath, bigBoi, allow_pickle=True, fix_imports=True)      
         
 def main():
-	# filename = input('Enter file for base image: ')
-	# size = input('Enter size of data set to generate ')
-	createSet('quarterNoLine.png',100)
+	filename = input('Enter file for base image: ')
+	size = input('Enter size of data set to generate ')
+	createSet(filename,int(size))
 
 if __name__ == '__main__':
 	main()
